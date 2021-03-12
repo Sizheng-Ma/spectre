40af1a9ad4432016fb582d294bde17190c7fed3d
Sizheng Ma <sma@caltech.edu>
2021-03-11 21:31:38 -0800

    perturbation in the cce grid

--- src/Evolution/Systems/Cce/Initialize/InverseCubic.cpp
+++ src/Evolution/Systems/Cce/Initialize/InverseCubic.cpp
@@ -19,6 +19,9 @@
 #include "Utilities/Gsl.hpp"
 #include "Utilities/TMPL.hpp"
 
+#include "Parallel/Printf.hpp"
+#include "Utilities/MakeString.hpp"
+
 namespace Cce::InitializeJ {
 
 std::unique_ptr<InitializeJ<true>> InverseCubic<true>::get_clone() const {
@@ -48,6 +51,19 @@ void InverseCubic<true>::operator()(
       1.0 - Spectral::collocation_points<Spectral::Basis::Legendre,
                                          Spectral::Quadrature::GaussLobatto>(
                 number_of_radial_points);
+  const size_t number_of_angular_points =
+      Spectral::Swsh::number_of_swsh_collocation_points(l_max);
+
+  Spectral::Swsh::SpinWeightedSphericalHarmonic y_22{2, 2_st, 0};
+  const auto& collocation_metadata =
+      Spectral::Swsh::cached_collocation_metadata<
+          Spectral::Swsh::ComplexRepresentation::Interleaved>(l_max);
+  SpinWeighted<ComplexDataVector, 2> perturbed_j{number_of_angular_points};
+  for (const auto collocation_point : collocation_metadata) {
+    const std::complex<double> y_22_factor =
+      y_22.evaluate(collocation_point.theta, collocation_point.phi);
+      perturbed_j.data()[collocation_point.offset] = y_22_factor;
+  }
   for (size_t i = 0; i < number_of_radial_points; i++) {
     ComplexDataVector angular_view_j{
         get(*j).data().data() + get(boundary_j).size() * i,
@@ -64,6 +80,18 @@ void InverseCubic<true>::operator()(
     angular_view_j =
         one_minus_y_collocation[i] * one_minus_y_coefficient +
         pow<3>(one_minus_y_collocation[i]) * one_minus_y_cubed_coefficient;
+
+double ycenter=-0.0;
+double ymin=-0.8;
+double ymax=0.8;
+double width=0.15;
+if(one_minus_y_collocation[i]>=(1.-ymax) &&
+   one_minus_y_collocation[i]<=(1.-ymin)){
+  angular_view_j+= perturbed_j.data()
+  * 0.0 * exp(-pow(1.0-one_minus_y_collocation[i]-ycenter,2.0)/width/width)
+  * (one_minus_y_collocation[i]-1.0+ymax)
+  * (1.-one_minus_y_collocation[i]-ymin) * 4.0 / pow((ymax-ymin),2.0)
+  ;}
   }
   Spectral::Swsh::create_angular_and_cartesian_coordinates(
       cartesian_cauchy_coordinates, angular_cauchy_coordinates, l_max);
