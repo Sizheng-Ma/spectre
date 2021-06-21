f2f0716cdeee593a84a82984295dae56f76ec6d3
Sizheng Ma <sma@caltech.edu>
2021-06-20 17:23:21 -0700

    add w_ccm to bjorhus new

--- src/Evolution/Systems/GeneralizedHarmonic/BoundaryConditions/BjorhusImpl.cpp
+++ src/Evolution/Systems/GeneralizedHarmonic/BoundaryConditions/BjorhusImpl.cpp
@@ -22,6 +22,8 @@
 #include "Utilities/GenerateInstantiations.hpp"
 #include "Utilities/Gsl.hpp"
 
+#include "Parallel/Printf.hpp"
+
 namespace GeneralizedHarmonic::BoundaryConditions::Bjorhus {
 template <size_t VolumeDim, typename DataType>
 void constraint_preserving_bjorhus_corrections_dt_v_psi(
@@ -502,6 +504,11 @@ void add_physical_terms_to_dt_v_minus(
       }
     }
   }
+  Parallel::printf("wccm1: %e U3m: %e U3p: %e\n",
+        w_ccm.get(1,2)[20],U3m.get(1,2)[20],U3p.get(1,2)[20]);
+  //Parallel::printf("wccm1: %zu U3m: %zu U3p: %zu\n",
+  //      w_ccm.get(1,2).size(),U3m.get(1,2).size(),U3p.get(1,2).size());
+
 }
 }  // namespace detail
 
