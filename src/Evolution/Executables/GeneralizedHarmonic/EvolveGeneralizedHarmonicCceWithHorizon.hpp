bb70318df8a2d63ff6b642fc72849156e859e47a
Sizheng Ma <sma@caltech.edu>
2021-08-03 17:48:21 -0700

    GH filter for BH

--- src/Evolution/Executables/GeneralizedHarmonic/EvolveGeneralizedHarmonicCceWithHorizon.hpp
+++ src/Evolution/Executables/GeneralizedHarmonic/EvolveGeneralizedHarmonicCceWithHorizon.hpp
@@ -72,6 +72,9 @@
 #include "Utilities/ErrorHandling/FloatingPointExceptions.hpp"
 #include "Utilities/TMPL.hpp"
 
+#include "NumericalAlgorithms/LinearOperators/ExponentialFilter.hpp"
+#include "NumericalAlgorithms/LinearOperators/FilterAction.hpp"
+
 /// \cond
 namespace Frame {
 // IWYU pragma: no_forward_declare MathFunction
@@ -177,7 +180,14 @@ struct EvolutionMetavars
       evolution::dg::Actions::ApplyBoundaryCorrections<EvolutionMetavars>,
       tmpl::conditional_t<
           local_time_stepping, tmpl::list<>,
-          tmpl::list<Actions::RecordTimeStepperData<>, Actions::UpdateU<>>>>;
+          tmpl::list<Actions::RecordTimeStepperData<>, Actions::UpdateU<>>>,
+        dg::Actions::Filter<
+          Filters::Exponential<0>,
+          tmpl::list<
+              gr::Tags::SpacetimeMetric<volume_dim, Frame::Inertial,
+                                        DataVector>,
+              GeneralizedHarmonic::Tags::Pi<volume_dim, Frame::Inertial>,
+              GeneralizedHarmonic::Tags::Phi<volume_dim, Frame::Inertial>>>>;
 
   using typename GeneralizedHarmonicTemplateBase<
       EvolutionMetavars>::analytic_solution_tag;
