fbf253f4c4c696e885c00b374aee61aa62f07c68
Sizheng Ma <sma@caltech.edu>
2022-01-27 22:36:09 -0800

    transfer TetradCoeffPhi and TetradCoeffTheta to GH

--- src/Evolution/Systems/GeneralizedHarmonic/BoundaryConditions/Bjorhus.hpp
+++ src/Evolution/Systems/GeneralizedHarmonic/BoundaryConditions/Bjorhus.hpp
@@ -176,8 +176,10 @@ class ConstraintPreservingBjorhus final : public BoundaryCondition<Dim> {
       ::Tags::deriv<Tags::Phi<Dim, Frame::Inertial>, tmpl::size_t<Dim>,
                     Frame::Inertial>>;
   using dg_gridless_tags = tmpl::list<
-                //GeneralizedHarmonic::Tags::CCMw<Dim, Frame::Inertial>,
-                Cce::Tags::BoundaryValue<Cce::Tags::Psi0Match>,Cce::Tags::LMax>;
+      // GeneralizedHarmonic::Tags::CCMw<Dim, Frame::Inertial>,
+      Cce::Tags::BoundaryValue<Cce::Tags::TetradCoeffTheta>,
+      Cce::Tags::BoundaryValue<Cce::Tags::TetradCoeffPhi>,
+      Cce::Tags::BoundaryValue<Cce::Tags::Psi0Match>, Cce::Tags::LMax>;
 
   std::optional<std::string> dg_time_derivative(
       gsl::not_null<tnsr::aa<DataVector, Dim, Frame::Inertial>*>
@@ -219,6 +221,8 @@ class ConstraintPreservingBjorhus final : public BoundaryCondition<Dim> {
       const tnsr::iaa<DataVector, Dim, Frame::Inertial>& d_spacetime_metric,
       const tnsr::iaa<DataVector, Dim, Frame::Inertial>& d_pi,
       const tnsr::ijaa<DataVector, Dim, Frame::Inertial>& d_phi,
+      const Scalar<SpinWeighted<ComplexDataVector, 0>>& tetrad_coeff_theta,
+      const Scalar<SpinWeighted<ComplexDataVector, 2>>& tetrad_coeff_phi,
       const Scalar<SpinWeighted<ComplexDataVector, 2>>& psi0,
       size_t l_max) const;
 
