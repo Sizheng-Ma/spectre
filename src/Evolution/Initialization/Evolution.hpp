180077da27ad6c3d2d40e4a2bf7f251842e42009
Sizheng Ma <sma@caltech.edu>
2022-09-26 21:11:23 -0700

    initializing ccm tags on GH

--- src/Evolution/Initialization/Evolution.hpp
+++ src/Evolution/Initialization/Evolution.hpp
@@ -11,6 +11,9 @@
 #include "DataStructures/DataBox/Prefixes.hpp"
 #include "Domain/Tags.hpp"
 #include "Evolution/Initialization/Tags.hpp"
+#include "Evolution/Systems/Cce/OptionTags.hpp"
+#include "Evolution/Systems/Cce/ReceiveTags.hpp"
+#include "Evolution/Systems/Cce/Tags.hpp"
 #include "NumericalAlgorithms/Spectral/Mesh.hpp"
 #include "Time/ChooseLtsStepSize.hpp"
 #include "Time/Slab.hpp"
@@ -137,6 +140,31 @@ struct TimeStepping {
     *next_time_step = *time_step;
   }
 };
+
+namespace Actions {
+template <typename Metavariables>
+struct InitializeCcmTags {
+  using tagtag = ::Tags::Variables<typename Metavariables::ccm_psi0>;
+  using simple_tags = tmpl::list<tagtag>;
+  using const_global_cache_tags = tmpl::list<Cce::Tags::LMax>;
+  template <typename DbTags, typename... InboxTags, typename ArrayIndex,
+            typename ActionList, typename ParallelComponent>
+  static Parallel::iterable_action_return_t apply(db::DataBox<DbTags>& box,
+                    const tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
+                    const Parallel::GlobalCache<Metavariables>& /*cache*/,
+                    const ArrayIndex& /*array_index*/,
+                    const ActionList /*meta*/,
+                    const ParallelComponent* const /*meta*/) {
+    const size_t l_max = db::get<Spectral::Swsh::Tags::LMaxBase>(box);
+    const size_t number_of_angular_grid_points =
+        Spectral::Swsh::number_of_swsh_collocation_points(l_max);
+    Initialization::mutate_assign<simple_tags>(
+        make_not_null(&box),
+        typename tagtag::type{number_of_angular_grid_points, 0.0});
+    return {Parallel::AlgorithmExecution::Continue, std::nullopt};
+  }
+};
+}
 
 /// \ingroup InitializationGroup
 /// \brief Initialize time-stepper items
