8454b1076cf0d921d8f7fd2158faa9916fe19a13
Sizheng Ma <sma@caltech.edu>
2023-06-17 15:31:12 -0700

    initialize_j

--- src/hello_world/hello_world.hpp
+++ src/hello_world/hello_world.hpp
@@ -8,6 +8,16 @@
 void myprint();
 int mynewfunction(int a);
 void print_data_vector();
+void initialize_j(std::vector<double>& re_j,std::vector<double>& im_j,
+                   const std::vector<double>& bondi_beta_spec,
+                   const std::vector<double>& bondi_dr_j_spec,
+                   const std::vector<double>& bondi_du_r_spec,
+                   const std::vector<double>& bondi_h_spec,
+                   const std::vector<double>& bondi_j_spec,
+                   const std::vector<double>& bondi_q_spec,
+                   const std::vector<double>& bondi_r_spec,
+                   const std::vector<double>& bondi_u_spec,
+                   const std::vector<double>& bondi_w_spec);
 void ccm_functions(std::vector<double>& psi0,
                    const std::vector<double>& bondi_beta_spec,
                    const std::vector<double>& bondi_dr_j_spec,
