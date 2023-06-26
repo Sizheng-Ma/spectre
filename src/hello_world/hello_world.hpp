30b8745b37a159891c142f6da489fd48faea2a61
Sizheng Ma <sma@caltech.edu>
2023-06-26 11:28:41 -0700

    std::complex<double> for ccm_functions

--- src/hello_world/hello_world.hpp
+++ src/hello_world/hello_world.hpp
@@ -20,15 +20,15 @@ void initialize_j(std::vector<double>& re_j, std::vector<double>& im_j,
                   const std::vector<std::complex<double>>& bondi_u_spec,
                   const std::vector<std::complex<double>>& bondi_w_spec);
 void ccm_functions(std::vector<double>& re_h, std::vector<double>& im_h,
-                   const std::vector<double>& bondi_beta_spec,
-                   const std::vector<double>& bondi_dr_j_spec,
-                   const std::vector<double>& bondi_du_r_spec,
-                   const std::vector<double>& bondi_h_spec,
-                   const std::vector<double>& bondi_j_spec,
-                   const std::vector<double>& bondi_q_spec,
-                   const std::vector<double>& bondi_r_spec,
-                   const std::vector<double>& bondi_u_spec,
-                   const std::vector<double>& bondi_w_spec);
+                   const std::vector<std::complex<double>>& bondi_beta_spec,
+                   const std::vector<std::complex<double>>& bondi_dr_j_spec,
+                   const std::vector<std::complex<double>>& bondi_du_r_spec,
+                   const std::vector<std::complex<double>>& bondi_h_spec,
+                   const std::vector<std::complex<double>>& bondi_j_spec,
+                   const std::vector<std::complex<double>>& bondi_q_spec,
+                   const std::vector<std::complex<double>>& bondi_r_spec,
+                   const std::vector<std::complex<double>>& bondi_u_spec,
+                   const std::vector<std::complex<double>>& bondi_w_spec);
 
 // struct test {
 //   using a = tmpl::list<>;
