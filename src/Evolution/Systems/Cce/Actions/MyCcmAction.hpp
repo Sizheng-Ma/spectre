8d100a193a55fb9f915322e0160e7f570375488a
Sizheng Ma <sma@caltech.edu>
2023-07-12 22:51:41 -0700

    MyCCMAction

--- src/Evolution/Systems/Cce/Actions/MyCcmAction.hpp
+++ src/Evolution/Systems/Cce/Actions/MyCcmAction.hpp
@@ -19,6 +19,23 @@ struct MyCCMAction {
       const Parallel::GlobalCache<Metavariables>& /*cache*/,
       const ArrayIndex& /*array_index*/, const ActionList /*meta*/,
       const ParallelComponent* const /*meta*/) {
+    // ccm_functions(std::vector<double>& re_h, std::vector<double>& im_h,
+    //            std::vector<double>& dt_cauchy_x,
+    //            std::vector<double>& dt_cauchy_y,
+    //            std::vector<double>& dt_cauchy_z,
+    //            std::vector<double>& dt_inertial_x,
+    //            std::vector<double>& dt_inertial_y,
+    //            std::vector<double>& dt_inertial_z,
+    //            std::vector<double>& re_psi3, std::vector<double>& im_psi3,
+    //            std::vector<double>& dt_u_scri, const size_t l_max,
+    //            const size_t number_of_radial_points,
+    //            const std::vector<std::vector<double>>& spacetime_metric,
+    //            const std::vector<std::vector<double>>& pi,
+    //            const std::vector<std::vector<std::vector<double>>>& phi,
+    //            const double radius, const std::vector<double>& re_j,
+    //            const std::vector<double>& im_j,
+    //            const std::vector<std::vector<double>>& cauchy_cart,
+    //            const std::vector<std::vector<double>>& inertial_cart);
     return {Parallel ::AlgorithmExecution::Continue, std::nullopt};
   }
 };
