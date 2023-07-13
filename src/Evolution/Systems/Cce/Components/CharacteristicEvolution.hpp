ef1b23a0f72515cc6c5f4555deaaec4fa3a5d5c0
Sizheng Ma <sma@caltech.edu>
2023-07-12 22:51:41 -0700

    MyCCMAction

--- src/Evolution/Systems/Cce/Components/CharacteristicEvolution.hpp
+++ src/Evolution/Systems/Cce/Components/CharacteristicEvolution.hpp
@@ -225,7 +225,7 @@ struct CharacteristicEvolution {
       tmpl::transform<bondi_hypersurface_step_tags,
                       tmpl::bind<hypersurface_computation, tmpl::_1>>,
       Actions::FilterSwshVolumeQuantity<Tags::BondiH>,
-      compute_scri_quantities_and_observe,
+      compute_scri_quantities_and_observe, Actions::MyCCMAction,
       ::Actions::RecordTimeStepperData<cce_system>,
       ::Actions::UpdateU<cce_system>,
       ::Actions::ChangeStepSize<typename Metavariables::cce_step_choosers>,
