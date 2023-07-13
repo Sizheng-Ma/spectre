f806c24c4c8bed24ed844e971ba792838f62b7ae
Sizheng Ma <sma@caltech.edu>
2023-07-12 23:05:36 -0700

    add const_global_cache_tags to MyCCMAction

--- src/Evolution/Systems/Cce/Actions/MyCcmAction.hpp
+++ src/Evolution/Systems/Cce/Actions/MyCcmAction.hpp
@@ -10,15 +10,21 @@ namespace Cce {
 namespace Actions {
 
 struct MyCCMAction {
+  using const_global_cache_tags =
+      tmpl::list<Tags::LMax, Tags::NumberOfRadialPoints,
+                 InitializationTags::ExtractionRadius>;
   template <typename DbTags, typename... InboxTags, typename Metavariables,
             typename ArrayIndex, typename ActionList,
             typename ParallelComponent>
   static Parallel::iterable_action_return_t apply(
       db::DataBox<DbTags>& box,
       const tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
-      const Parallel::GlobalCache<Metavariables>& /*cache*/,
+      const Parallel::GlobalCache<Metavariables>& cache,
       const ArrayIndex& /*array_index*/, const ActionList /*meta*/,
       const ParallelComponent* const /*meta*/) {
+    auto l_max = db::get<Tags::LMax>(box);
+    auto number_of_radial_points = db::get<Tags::NumberOfRadialPoints>(box);
+    auto radius = db::get<InitializationTags::ExtractionRadius>(box);
     // ccm_functions(std::vector<double>& re_h, std::vector<double>& im_h,
     //            std::vector<double>& dt_cauchy_x,
     //            std::vector<double>& dt_cauchy_y,
