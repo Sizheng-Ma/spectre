1aa932b1029532ccca5f3549d09642ed6d37809d
Sizheng Ma <sma@caltech.edu>
2023-07-14 13:20:58 -0700

    print more

--- src/hello_world/hello_world_test.hpp
+++ src/hello_world/hello_world_test.hpp
@@ -9,9 +9,12 @@
 #include "Evolution/Systems/Cce/Tags.hpp"
 
 void ccm_functions11(
-    std::vector<double>& re_h, std::vector<double>& im_h, const size_t l_max,
-    const size_t number_of_radial_points, const double radius,
-    const std::vector<double>& re_j, const std::vector<double>& im_j,
+    std::vector<double>& re_h, std::vector<double>& im_h,
+    std::vector<double>& dt_cauchy_x, std::vector<double>& dt_cauchy_y,
+    std::vector<double>& dt_cauchy_z, std::vector<double>& dt_u_scri,
+    const size_t l_max, const size_t number_of_radial_points,
+    const double radius, const std::vector<double>& re_j,
+    const std::vector<double>& im_j,
     const std::vector<std::vector<double>>& cauchy_cart,
     const std::vector<std::vector<double>>& inertial_cart,
     const Cce::Tags::BoundaryValue<Cce::Tags::BondiBeta>::type& cce_bondi_beta,
