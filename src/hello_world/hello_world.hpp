7deb45f6e28eb2c1c18aa728f7ac21e3c9164e4f
Sizheng Ma <sma@caltech.edu>
2023-07-18 13:35:59 -0700

    psis_to_write

--- src/hello_world/hello_world.hpp
+++ src/hello_world/hello_world.hpp
@@ -65,7 +65,12 @@ struct InterpolationInterface {
       std::vector<std::complex<double>>& news,
       std::vector<std::complex<double>>& eth_inertial_retarded_time);
 
-  void ScriObserveInterpolated(std::vector<double>& strain_to_write, std::vector<double>& news_to_write);
+  void ScriObserveInterpolated(
+      std::vector<double>& eth_inertial_retarded_time_to_write,
+      std::vector<double>& psi0_to_write, std::vector<double>& psi1_to_write,
+      std::vector<double>& psi2_to_write, std::vector<double>& psi3_to_write,
+      std::vector<double>& psi4_to_write, std::vector<double>& strain_to_write,
+      std::vector<double>& news_to_write);
 
  private:
   MyScriPlusInterpolationManager* my_scri_plus_interpolation_manager_;
