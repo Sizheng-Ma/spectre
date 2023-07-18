a13ab1df70b75492b82af221c61462ac13c205c2
Sizheng Ma <sma@caltech.edu>
2023-07-18 08:19:33 -0700

    take strain_to_write

--- src/hello_world/hello_world.hpp
+++ src/hello_world/hello_world.hpp
@@ -65,7 +65,7 @@ struct InterpolationInterface {
       std::vector<std::complex<double>>& news,
       std::vector<std::complex<double>>& eth_inertial_retarded_time);
 
-  void ScriObserveInterpolated();
+  void ScriObserveInterpolated(std::vector<double>& strain_to_write);
 
  private:
   MyScriPlusInterpolationManager* my_scri_plus_interpolation_manager_;
