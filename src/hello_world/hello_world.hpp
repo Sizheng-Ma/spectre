0bbc02cafa4a79ad49d02f82b0b64d99d6cbd214
Sizheng Ma <sma@caltech.edu>
2023-07-21 09:28:46 -0700

    dump psi4

--- src/hello_world/hello_world.hpp
+++ src/hello_world/hello_world.hpp
@@ -25,23 +25,21 @@ void initialize_j(std::vector<double>& re_j, std::vector<double>& im_j,
                   const std::vector<std::vector<double>>& pi,
                   const std::vector<std::vector<std::vector<double>>>& phi,
                   const double radius);
-void ccm_functions(std::vector<double>& re_h, std::vector<double>& im_h,
-                   std::vector<double>& dt_cauchy_x,
-                   std::vector<double>& dt_cauchy_y,
-                   std::vector<double>& dt_cauchy_z,
-                   std::vector<double>& dt_inertial_x,
-                   std::vector<double>& dt_inertial_y,
-                   std::vector<double>& dt_inertial_z,
-                   std::vector<double>& re_psi3, std::vector<double>& im_psi3,
-                   std::vector<double>& dt_u_scri, const size_t l_max,
-                   const size_t number_of_radial_points,
-                   const std::vector<std::vector<double>>& spacetime_metric,
-                   const std::vector<std::vector<double>>& pi,
-                   const std::vector<std::vector<std::vector<double>>>& phi,
-                   const double radius, const std::vector<double>& re_j,
-                   const std::vector<double>& im_j,
-                   const std::vector<std::vector<double>>& cauchy_cart,
-                   const std::vector<std::vector<double>>& inertial_cart);
+void ccm_functions(
+    std::vector<double>& re_h, std::vector<double>& im_h,
+    std::vector<double>& dt_cauchy_x, std::vector<double>& dt_cauchy_y,
+    std::vector<double>& dt_cauchy_z, std::vector<double>& dt_inertial_x,
+    std::vector<double>& dt_inertial_y, std::vector<double>& dt_inertial_z,
+    std::vector<double>& re_psi3, std::vector<double>& im_psi3,
+    std::vector<std::complex<double>>& psi4, std::vector<double>& dt_u_scri,
+    const size_t l_max, const size_t number_of_radial_points,
+    const std::vector<std::vector<double>>& spacetime_metric,
+    const std::vector<std::vector<double>>& pi,
+    const std::vector<std::vector<std::vector<double>>>& phi,
+    const double radius, const std::vector<double>& re_j,
+    const std::vector<double>& im_j,
+    const std::vector<std::vector<double>>& cauchy_cart,
+    const std::vector<std::vector<double>>& inertial_cart);
 
 namespace spectre {
 
