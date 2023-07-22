329b772e8d837180476440c5a9d12b7179eaa3ff
Sizheng Ma <sma@caltech.edu>
2023-07-21 18:31:37 -0700

    use ccm_functions

--- src/Evolution/Systems/Cce/Actions/MyCcmAction.hpp
+++ src/Evolution/Systems/Cce/Actions/MyCcmAction.hpp
@@ -14,66 +14,6 @@ struct MyCCMAction {
   using const_global_cache_tags =
       tmpl::list<Tags::LMax, Tags::NumberOfRadialPoints>;
 
-  void DataVector_to_std_vector(const tnsr::aa<DataVector, 3>& pi,
-                                std::vector<std::vector<double>>& data) {
-    const auto size = get<0, 0>(pi).size();
-    std::vector<double> datattt(size);
-    std::vector<double> datattx(size);
-    std::vector<double> datatty(size);
-    std::vector<double> datattz(size);
-    std::vector<double> datatxx(size);
-    std::vector<double> datatxy(size);
-    std::vector<double> datatxz(size);
-    std::vector<double> datatyy(size);
-    std::vector<double> datatyz(size);
-    std::vector<double> datatzz(size);
-    for (unsigned int i = 0; i < size; i++) {
-      datattt[i] = get<0, 0>(pi)[i];
-      datattx[i] = get<0, 1>(pi)[i];
-      datatty[i] = get<0, 2>(pi)[i];
-      datattz[i] = get<0, 3>(pi)[i];
-      datatxx[i] = get<1, 1>(pi)[i];
-      datatxy[i] = get<1, 2>(pi)[i];
-      datatxz[i] = get<1, 3>(pi)[i];
-      datatyy[i] = get<2, 2>(pi)[i];
-      datatyz[i] = get<2, 3>(pi)[i];
-      datatzz[i] = get<3, 3>(pi)[i];
-    }
-  }
-
-  void DataVector_to_tri_std_vector(
-      const tnsr::iaa<DataVector, 3>& pi,
-      std::vector<std::vector<std::vector<double>>>& data) {
-    const auto size = pi.get(0, 0, 0).size();
-    std::vector<double> datattt(size);
-    std::vector<double> datattx(size);
-    std::vector<double> datatty(size);
-    std::vector<double> datattz(size);
-    std::vector<double> datatxx(size);
-    std::vector<double> datatxy(size);
-    std::vector<double> datatxz(size);
-    std::vector<double> datatyy(size);
-    std::vector<double> datatyz(size);
-    std::vector<double> datatzz(size);
-    for (size_t ijj = 0; ijj < 3; ++ijj) {
-      for (size_t i = 0; i < size; i++) {
-        datattt[i] = pi.get(ijj, 0, 0)[i];
-        datattx[i] = pi.get(ijj, 0, 1)[i];
-        datatty[i] = pi.get(ijj, 0, 2)[i];
-        datattz[i] = pi.get(ijj, 0, 3)[i];
-        datatxx[i] = pi.get(ijj, 1, 1)[i];
-        datatxy[i] = pi.get(ijj, 1, 2)[i];
-        datatxz[i] = pi.get(ijj, 1, 3)[i];
-        datatyy[i] = pi.get(ijj, 2, 2)[i];
-        datatyz[i] = pi.get(ijj, 2, 3)[i];
-        datatzz[i] = pi.get(ijj, 3, 3)[i];
-      }
-      std::vector<std::vector<double>> datafinal{
-          datattt, datattx, datatty, datattz, datatxx,
-          datatxy, datatxz, datatyy, datatyz, datatzz};
-      data.push_back(datafinal);
-    }
-  }
   template <typename DbTags, typename... InboxTags, typename Metavariables,
             typename ArrayIndex, typename ActionList,
             typename ParallelComponent>
@@ -86,7 +26,7 @@ struct MyCCMAction {
     auto l_max = db::get<Tags::LMax>(box);
     auto number_of_radial_points = db::get<Tags::NumberOfRadialPoints>(box);
 
-    double radius;
+    double radius = 41.;
     if constexpr (tt::is_a_v<AnalyticWorldtubeBoundary, Boundary>) {
       radius = db::get<Tags::AnalyticBoundaryDataManager>(box)
                    .get_extraction_radius();
@@ -100,15 +40,13 @@ struct MyCCMAction {
 
     const size_t boundary_size =
         Spectral::Swsh::number_of_swsh_collocation_points(l_max);
-    std::vector<double> test(boundary_size, 1.);
-    std::vector<double> mtest(boundary_size, -1.);
-    std::vector<double> zero(boundary_size, 0);
+    // std::vector<double> test(boundary_size, 1.);
+    // std::vector<double> mtest(boundary_size, -1.);
+    // std::vector<double> zero(boundary_size, 0);
 
-    std::vector<std::vector<double>> pi{zero, zero, zero, zero, zero,
-                                        zero, zero, zero, zero, zero};
-    std::vector<std::vector<double>> spacetime_metric{
-        mtest, zero, zero, zero, test, zero, zero, test, zero, test};
-    std::vector<std::vector<std::vector<double>>> phi{pi, pi, pi};
+    std::vector<std::vector<double>> pi;
+    std::vector<std::vector<double>> spacetime_metric;
+    std::vector<std::vector<std::vector<double>>> phi;
 
     std::vector<double> re_j;
     std::vector<double> im_j;
@@ -155,6 +93,10 @@ struct MyCCMAction {
     auto& my_phi = db::get<Cce::Tags::TestPhi>(box);
     auto& my_pi = db::get<Cce::Tags::TestPi>(box);
 
+    ThisThisDataVector_to_std_vector(my_space_time, spacetime_metric);
+    ThisThisDataVector_to_std_vector(my_pi, pi);
+    ThisThisDataVector_to_tri_std_vector(my_phi, phi);
+
     // auto& bondi_beta_bdry =
     //     db::get<Cce::Tags::BoundaryValue<Cce::Tags::BondiBeta>>(box);
 
@@ -199,14 +141,22 @@ struct MyCCMAction {
     // Metavariables::cce_boundary_communication_tags>>(box);
 
     std::vector<double> dt_cauchy_x, dt_cauchy_y, dt_cauchy_z, dt_u_scri;
+    std::vector<double> dt_inertial_x, dt_inertial_y, dt_inertial_z;
 
     // ccm_functions11(
-    //     re_h, im_h, dt_cauchy_x, dt_cauchy_y, dt_cauchy_z, dt_u_scri, l_max,
-    //     number_of_radial_points, radius, re_j, im_j, cauchy_cart_std,
-    //     inertial_cart_std, bondi_beta_bdry, dr_j_bdry, du_r_bdry,
-    //     bondi_h_bdry, bondi_j_bdry, bondi_q_bdry, bondi_r_bdry, bondi_u_bdry,
-    //     bondi_w_bdry, bondi_dr_u_bdry, bondi_du_j_bdry,
+    //     re_h, im_h, dt_cauchy_x, dt_cauchy_y, dt_cauchy_z, dt_u_scri,
+    //     l_max, number_of_radial_points, radius, re_j, im_j,
+    //     cauchy_cart_std, inertial_cart_std, bondi_beta_bdry, dr_j_bdry,
+    //     du_r_bdry, bondi_h_bdry, bondi_j_bdry, bondi_q_bdry, bondi_r_bdry,
+    //     bondi_u_bdry, bondi_w_bdry, bondi_dr_u_bdry, bondi_du_j_bdry,
     //     bondi_du_r_bdry_DuRDividedByR);
+
+    ccm_functions(re_h, im_h, dt_cauchy_x, dt_cauchy_y, dt_cauchy_z,
+                  dt_inertial_x, dt_inertial_y, dt_inertial_z,
+                  eth_inertial_retarded_time, news, strain, psi0, psi1, psi2,
+                  psi3, psi4, dt_u_scri, l_max, number_of_radial_points,
+                  spacetime_metric, pi, phi, radius, re_j, im_j,
+                  cauchy_cart_std, inertial_cart_std);
 
     auto bondi_h = db::get<Tags::BondiH>(box);
 
@@ -250,6 +200,77 @@ struct MyCCMAction {
     // std::cout << "my beta " << std::setprecision(30)
     //           << get(dt_cauchy_cart).data()[0] << std::endl;
     return {Parallel ::AlgorithmExecution::Continue, std::nullopt};
+  }
+  static void ThisThisDataVector_to_std_vector(
+      const tnsr::aa<DataVector, 3>& pi,
+      std::vector<std::vector<double>>& data) {
+    const auto size = get<0, 0>(pi).size();
+    std::vector<double> datattt(size);
+    std::vector<double> datattx(size);
+    std::vector<double> datatty(size);
+    std::vector<double> datattz(size);
+    std::vector<double> datatxx(size);
+    std::vector<double> datatxy(size);
+    std::vector<double> datatxz(size);
+    std::vector<double> datatyy(size);
+    std::vector<double> datatyz(size);
+    std::vector<double> datatzz(size);
+    for (unsigned int i = 0; i < size; i++) {
+      datattt[i] = get<0, 0>(pi)[i];
+      datattx[i] = get<0, 1>(pi)[i];
+      datatty[i] = get<0, 2>(pi)[i];
+      datattz[i] = get<0, 3>(pi)[i];
+      datatxx[i] = get<1, 1>(pi)[i];
+      datatxy[i] = get<1, 2>(pi)[i];
+      datatxz[i] = get<1, 3>(pi)[i];
+      datatyy[i] = get<2, 2>(pi)[i];
+      datatyz[i] = get<2, 3>(pi)[i];
+      datatzz[i] = get<3, 3>(pi)[i];
+    }
+    data.push_back(datattt);
+    data.push_back(datattx);
+    data.push_back(datatty);
+    data.push_back(datattz);
+    data.push_back(datatxx);
+    data.push_back(datatxy);
+    data.push_back(datatxz);
+    data.push_back(datatyy);
+    data.push_back(datatyz);
+    data.push_back(datatzz);
+  }
+
+  static void ThisThisDataVector_to_tri_std_vector(
+      const tnsr::iaa<DataVector, 3>& pi,
+      std::vector<std::vector<std::vector<double>>>& data) {
+    const auto size = pi.get(0, 0, 0).size();
+    std::vector<double> datattt(size);
+    std::vector<double> datattx(size);
+    std::vector<double> datatty(size);
+    std::vector<double> datattz(size);
+    std::vector<double> datatxx(size);
+    std::vector<double> datatxy(size);
+    std::vector<double> datatxz(size);
+    std::vector<double> datatyy(size);
+    std::vector<double> datatyz(size);
+    std::vector<double> datatzz(size);
+    for (size_t ijj = 0; ijj < 3; ++ijj) {
+      for (size_t i = 0; i < size; i++) {
+        datattt[i] = pi.get(ijj, 0, 0)[i];
+        datattx[i] = pi.get(ijj, 0, 1)[i];
+        datatty[i] = pi.get(ijj, 0, 2)[i];
+        datattz[i] = pi.get(ijj, 0, 3)[i];
+        datatxx[i] = pi.get(ijj, 1, 1)[i];
+        datatxy[i] = pi.get(ijj, 1, 2)[i];
+        datatxz[i] = pi.get(ijj, 1, 3)[i];
+        datatyy[i] = pi.get(ijj, 2, 2)[i];
+        datatyz[i] = pi.get(ijj, 2, 3)[i];
+        datatzz[i] = pi.get(ijj, 3, 3)[i];
+      }
+      std::vector<std::vector<double>> datafinal{
+          datattt, datattx, datatty, datattz, datatxx,
+          datatxy, datatxz, datatyy, datatyz, datatzz};
+      data.push_back(datafinal);
+    }
   }
 };
 }  // namespace Actions
