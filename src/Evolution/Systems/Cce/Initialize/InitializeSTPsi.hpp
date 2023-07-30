6cd5e1eab1a045c574a3b2c325640ea441ed9c29
Sizheng Ma <sma@caltech.edu>
2023-07-30 15:36:38 -0700

    add InitializeSTPsi

---
+++ src/Evolution/Systems/Cce/Initialize/InitializeSTPsi.hpp
@@ -0,0 +1,29 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include "Evolution/Systems/Cce/Tags.hpp"
+
+namespace Cce {
+
+namespace Tags {
+/// \cond
+struct LMax;
+struct NumberOfRadialPoints;
+/// \endcond
+}  // namespace Tags
+
+namespace ScalarTensor {
+
+struct InitializeSTPsi {
+  using return_tags = tmpl::list<Tags::BondiSTPsi>;
+  using argument_tags = tmpl::list<Tags::NumberOfRadialPoints, Tags::LMax>;
+
+  static void apply(
+      gsl::not_null<Scalar<SpinWeighted<ComplexDataVector, 0>>*> bondi_st_psi,
+      const size_t l_max, const size_t number_of_radial_points);
+};
+
+}  // namespace ScalarTensor
+}  // namespace Cce
