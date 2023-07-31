390a36a71fc7a7ddd3f68c4595a83e72f4a205c7
Sizheng Ma <sma@caltech.edu>
2023-07-30 20:43:40 -0700

    minor

--- src/Evolution/Systems/Cce/Equations.cpp
+++ src/Evolution/Systems/Cce/Equations.cpp
@@ -34,8 +34,10 @@ void ComputeBondiIntegrand<Tags::RegularIntegrand<Tags::BondiSTTheta>>::
     apply_impl(gsl::not_null<SpinWeighted<ComplexDataVector, 0>*>
                    regular_integrand_for_st_theta,
                const SpinWeighted<ComplexDataVector, 0>& dy_dy_st_psi,
+               const SpinWeighted<ComplexDataVector, 2>& j,
                const SpinWeighted<ComplexDataVector, 0>& du_r_divided_by_r,
-               const SpinWeighted<ComplexDataVector, 0>& one_minus_y) {
+               const SpinWeighted<ComplexDataVector, 0>& one_minus_y,
+               const SpinWeighted<ComplexDataVector, 1>& eth_r_divided_by_r) {
   SpinWeighted<ComplexDataVector, 0> from_lhs =
       du_r_divided_by_r * one_minus_y * dy_dy_st_psi;
 }
