42a657c827e01d329ff37f1b9a23edd2f8425025
Sizheng Ma <sma@caltech.edu>
2023-07-30 22:07:36 -0700

    minor

--- src/Evolution/Systems/Cce/Equations.hpp
+++ src/Evolution/Systems/Cce/Equations.hpp
@@ -283,7 +283,9 @@ struct ComputeBondiIntegrand<Tags::RegularIntegrand<Tags::BondiSTTheta>> {
                  Spectral::Swsh::Tags::Derivative<Tags::BondiSTPsi,
                                                   Spectral::Swsh::Tags::EthEth>,
                  Spectral::Swsh::Tags::Derivative<Tags::BondiSTPsi,
-                                                  Spectral::Swsh::Tags::Eth>>;
+                                                  Spectral::Swsh::Tags::Eth>,
+                 Spectral::Swsh::Tags::Derivative<
+                     Tags::BondiJ, Spectral::Swsh::Tags::Ethbar>>;
   using integration_independent_tags =
       tmpl::list<Tags::DuRDividedByR, Tags::OneMinusY, Tags::EthRDividedByR,
                  Tags::EthEthRDividedByR, Tags::BondiR, Tags::BondiK>;
@@ -316,6 +318,7 @@ struct ComputeBondiIntegrand<Tags::RegularIntegrand<Tags::BondiSTTheta>> {
       const SpinWeighted<ComplexDataVector, 1>& eth_dy_st_psi,
       const SpinWeighted<ComplexDataVector, 2>& eth_eth_st_psi,
       const SpinWeighted<ComplexDataVector, 1>& eth_st_psi,
+      const SpinWeighted<ComplexDataVector, 1>& ethbar_j,
       // swsh_derivative_tags
       const SpinWeighted<ComplexDataVector, 0>& du_r_divided_by_r,
       const SpinWeighted<ComplexDataVector, 0>& one_minus_y,
