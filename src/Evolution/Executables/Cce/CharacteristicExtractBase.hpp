70105d5a1a3b7acb4764fc06d31442e5307e87fc
Sizheng Ma <sma@caltech.edu>
2023-07-30 18:56:58 -0700

    ComputeBondiIntegrand

--- src/Evolution/Executables/Cce/CharacteristicExtractBase.hpp
+++ src/Evolution/Executables/Cce/CharacteristicExtractBase.hpp
@@ -87,6 +87,10 @@ struct CharacteristicExtractDefaults {
       Cce::bondi_hypersurface_step_tags,
       tmpl::bind<Cce::integrand_terms_to_compute_for_bondi_variable,
                  tmpl::_1>>>;
+
+  using cce_st_integrand_tags =
+      Cce::integrand_terms_to_compute_for_bondi_variable<
+          Cce::Tags::BondiSTTheta>;
   using ccm_matching_tags = tmpl::list<
       Cce::Tags::BondiJCauchyView, Cce::Tags::Psi0Match,
       Cce::Tags::Dy<Cce::Tags::Psi0Match>,
