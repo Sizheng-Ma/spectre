3d3ee5b58fe0b4c15b3dc54f0854cb670b81f24a
Sizheng Ma <sma@caltech.edu>
2023-08-06 21:02:15 -0700

    read ST

--- src/Evolution/Systems/Cce/BoundaryData.hpp
+++ src/Evolution/Systems/Cce/BoundaryData.hpp
@@ -548,6 +548,10 @@ using characteristic_worldtube_boundary_tags = db::wrap_tags_in<
                Tags::BondiQ, Tags::BondiW, Tags::BondiJ, Tags::Dr<Tags::BondiJ>,
                Tags::BondiH, Tags::Du<Tags::BondiJ>, Tags::BondiR,
                Tags::Du<Tags::BondiR>, Tags::DuRDividedByR>>;
+
+template <template <typename> class BoundaryPrefix>
+using st_worldtube_boundary_tags =
+    db::wrap_tags_in<BoundaryPrefix, tmpl::list<Tags::BondiSTPsi>>;
 }  // namespace Tags
 
 namespace detail {
