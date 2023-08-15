70d4e2de057f04540dd5dcf619aa7e81c5247e96
Sizheng Ma <sma@caltech.edu>
2023-08-15 17:05:09 -0400

    Tags::Integrand<Tags::BondiSTduX>

--- src/Evolution/Systems/Cce/Equations.cpp
+++ src/Evolution/Systems/Cce/Equations.cpp
@@ -36,6 +36,18 @@ void ComputeBondiIntegrand<Tags::PoleOfIntegrand<Tags::BondiSTTheta>>::
       0.5 * (-eth_st_psi * conj(bondi_u) - conj(eth_st_psi) * bondi_u);
 }
 
+void ComputeBondiIntegrand<Tags::Integrand<Tags::BondiSTduX>>::apply_impl(
+    gsl::not_null<SpinWeighted<ComplexDataVector, 0>*> integrand_for_duX,
+    const SpinWeighted<ComplexDataVector, 0>& ethethbar_st_X,
+    const SpinWeighted<ComplexDataVector, 0>& one_minus_y,
+    const SpinWeighted<ComplexDataVector, 0>& beta,
+    const SpinWeighted<ComplexDataVector, 0>& bondi_r,
+    const SpinWeighted<ComplexDataVector, 0>& bondi_st_X) {
+  *integrand_for_duX = bondi_st_X * one_minus_y / bondi_r * exp(-4. * beta) +
+                       exp(2. * beta) * ethethbar_st_X;
+  *integrand_for_duX = *integrand_for_duX / (2. * bondi_r);
+}
+
 namespace detail {
 
 void BH(SpinWeighted<ComplexDataVector, 0>& result,
