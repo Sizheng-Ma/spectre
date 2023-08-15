e0ff6cbba057ec2e78b2da4c838624f7ca349df0
Sizheng Ma <sma@caltech.edu>
2023-08-15 15:44:15 -0400

    InitializeSTX

--- src/Evolution/Systems/Cce/Initialize/InitializeSTPsi.hpp
+++ src/Evolution/Systems/Cce/Initialize/InitializeSTPsi.hpp
@@ -17,13 +17,14 @@ struct NumberOfRadialPoints;
 namespace ScalarTensor {
 
 struct InitializeSTPsi {
-  using return_tags = tmpl::list<Tags::BondiSTPsi>;
+  using return_tags = tmpl::list<Tags::BondiSTPsi, Tags::BondiSTX>;
   using argument_tags = tmpl::list<Tags::LMax, Tags::NumberOfRadialPoints,
                                    Tags::BoundaryValue<Tags::BondiSTPsi>,
                                    Tags::BoundaryValue<Tags::BondiR>>;
 
   static void apply(
       gsl::not_null<Scalar<SpinWeighted<ComplexDataVector, 0>>*> bondi_st_psi,
+      gsl::not_null<Scalar<SpinWeighted<ComplexDataVector, 0>>*> bondi_st_x,
       const size_t l_max, const size_t number_of_radial_points,
       const Scalar<SpinWeighted<ComplexDataVector, 0>> st_psi_boundary,
       const Scalar<SpinWeighted<ComplexDataVector, 0>> bondi_r);
