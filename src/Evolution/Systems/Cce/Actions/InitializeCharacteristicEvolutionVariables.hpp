cd59d6c7698fc8274f142e5112e9b992a3682cdd
Sizheng Ma <sma@caltech.edu>
2023-08-15 16:20:12 -0400

    boundary of duX

--- src/Evolution/Systems/Cce/Actions/InitializeCharacteristicEvolutionVariables.hpp
+++ src/Evolution/Systems/Cce/Actions/InitializeCharacteristicEvolutionVariables.hpp
@@ -79,6 +79,8 @@ struct InitializeCharacteristicEvolutionVariables {
       typename Metavariables::st_cce_boundary_communication_tags>;
   using st_gauge_boundary_variable_tag =
       ::Tags::Variables<typename Metavariables::st_gauge_boundary_tag>;
+  using st_gauge_boundary_variable_ana_tag =
+      ::Tags::Variables<tmpl::list<Cce::Tags::EvolutionGaugeBoundaryValue<Cce::Tags::BondiSTduX>>>;
   using scri_variables_tag =
       ::Tags::Variables<typename Metavariables::cce_scri_tags>;
   using st_scri_variables_tag =
@@ -119,7 +121,7 @@ struct InitializeCharacteristicEvolutionVariables {
       Spectral::Swsh::Tags::SwshInterpolator<Tags::CauchyAngularCoords>,
       Spectral::Swsh::Tags::SwshInterpolator<Tags::PartiallyFlatAngularCoords>,
       ccm_tag, st_scri_variables_tag, st_boundary_value_variables_tag,
-      st_gauge_boundary_variable_tag>;
+      st_gauge_boundary_variable_tag,st_gauge_boundary_variable_ana_tag>;
   using simple_tags =
       tmpl::append<StepChoosers::step_chooser_simple_tags<Metavariables, true>,
                    simple_tags_for_evolution>;
@@ -169,7 +171,7 @@ struct InitializeCharacteristicEvolutionVariables {
         typename ccm_tag::type{boundary_size},
         typename st_scri_variables_tag::type{boundary_size},
         typename st_boundary_value_variables_tag::type{boundary_size},
-        typename st_gauge_boundary_variable_tag::type{boundary_size});
+        typename st_gauge_boundary_variable_tag::type{boundary_size},typename st_gauge_boundary_variable_ana_tag::type{boundary_size});
   }
 };
 
