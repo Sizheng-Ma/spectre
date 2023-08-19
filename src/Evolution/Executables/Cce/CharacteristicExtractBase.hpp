6ad9a6735a33a78bfb2c7e5e4cddafc3a2814708
Sizheng Ma <sma@caltech.edu>
2023-08-18 20:48:39 -0400

    communicate Dr_psi

--- src/Evolution/Executables/Cce/CharacteristicExtractBase.hpp
+++ src/Evolution/Executables/Cce/CharacteristicExtractBase.hpp
@@ -45,10 +45,11 @@ struct CharacteristicExtractDefaults {
       Cce::Tags::characteristic_worldtube_boundary_tags<
           Cce::Tags::BoundaryValue>;
 
-  using st_cce_boundary_communication_tags =
-      tmpl::list<Cce::Tags::BoundaryValue<Cce::Tags::BondiSTPsi>,
-                 Cce::Tags::BoundaryValue<Cce::Tags::BondiSTTheta>,
-                 Cce::Tags::BoundaryValue<Cce::Tags::BondiSTduX>>;
+  using st_cce_boundary_communication_tags = tmpl::list<
+      Cce::Tags::BoundaryValue<Cce::Tags::BondiSTPsi>,
+      Cce::Tags::BoundaryValue<Cce::Tags::BondiSTTheta>,
+      Cce::Tags::BoundaryValue<Cce::Tags::BondiSTduX>,
+      Cce::Tags::BoundaryValue<Cce::Tags::Dr<Cce::Tags::BondiSTPsi>>>;
 
   using st_gauge_boundary_tag = tmpl::list<
       Cce::Tags::EvolutionGaugeBoundaryValue<Cce::Tags::BondiSTTheta>>;
