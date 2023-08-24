043cfe9155d8233d8e9d520bdcf8e08dcb7594c0
Sizheng Ma <sma@caltech.edu>
2023-08-24 13:44:00 -0400

    BondiSTPsi to WorldtubeData

--- src/Evolution/Systems/Cce/AnalyticSolutions/SphericalMetricData.hpp
+++ src/Evolution/Systems/Cce/AnalyticSolutions/SphericalMetricData.hpp
@@ -245,6 +245,21 @@ struct SphericalMetricData : public WorldtubeData {
       double time,
       tmpl::type_<gh::Tags::Phi<DataVector, 3>> /*meta*/) const override;
 
+  void variables_impl(
+      gsl::not_null<Scalar<SpinWeighted<ComplexDataVector, 0>>*> st_psi,
+      size_t output_l_max, double time,
+      tmpl::type_<Tags::BondiSTPsi> /*meta*/) const override;
+
+  void variables_impl(
+      gsl::not_null<Scalar<SpinWeighted<ComplexDataVector, 0>>*> st_psi,
+      size_t output_l_max, double time,
+      tmpl::type_<Tags::Dr<Tags::BondiSTPsi>> /*meta*/) const override;
+
+  void variables_impl(
+      gsl::not_null<Scalar<SpinWeighted<ComplexDataVector, 0>>*> st_psi,
+      size_t output_l_max, double time,
+      tmpl::type_<Tags::BondiSTTheta> /*meta*/) const override;
+
   /// Must be overriden in the derived class; should compute the spacetime
   /// metric of the analytic solution in spherical coordinates.
   virtual void spherical_metric(
