56a53902833dbcedea88c7386291764684f8fc4b
Sizheng Ma <sma@caltech.edu>
2023-09-02 15:11:19 -0400

    GaugeAdjustedBoundaryValue

--- src/Evolution/Systems/Cce/GaugeTransformBoundaryData.cpp
+++ src/Evolution/Systems/Cce/GaugeTransformBoundaryData.cpp
@@ -17,6 +17,28 @@
 
 namespace Cce {
 
+namespace hihihi {
+template <int N>
+void compute_norm(
+    const SpinWeighted<ComplexDataVector, N> to_compare,
+    const SpinWeighted<ComplexDataVector, N> regular_integrand_for_st_theta) {
+  SpinWeighted<ComplexDataVector, N> final_diff =
+      to_compare - (regular_integrand_for_st_theta);
+
+  double norm = 0;
+
+  for (size_t iiiii = 0; iiiii < final_diff.size(); iiiii++) {
+    norm += square(abs(final_diff.data()[iiiii]));
+  }
+
+  norm /= final_diff.size();
+
+  norm = sqrt(norm);
+
+  std::cout << norm << std::endl;
+}
+}  // namespace hihihi
+
 void GaugeAdjustedBoundaryValue<Tags::BondiR>::apply(
     const gsl::not_null<Scalar<SpinWeighted<ComplexDataVector, 0>>*>
         evolution_gauge_r,
@@ -115,28 +137,6 @@ void GaugeAdjustedBoundaryValue<Tags::BondiBeta>::apply(
   get(*evolution_gauge_beta).data() -= 0.5 * log(get(omega).data());
 }
 
-namespace hihihi {
-template <int N>
-void compute_norm(
-    const SpinWeighted<ComplexDataVector, N> to_compare,
-    const SpinWeighted<ComplexDataVector, N> regular_integrand_for_st_theta) {
-  SpinWeighted<ComplexDataVector, N> final_diff =
-      to_compare - (regular_integrand_for_st_theta);
-
-  double norm = 0;
-
-  for (size_t iiiii = 0; iiiii < final_diff.size(); iiiii++) {
-    norm += square(abs(final_diff.data()[iiiii]));
-  }
-
-  norm /= final_diff.size();
-
-  norm = sqrt(norm);
-
-  std::cout << norm << std::endl;
-}
-}  // namespace hihihi
-
 void GaugeAdjustedBoundaryValue<Tags::BondiSTTheta>::apply(
     gsl::not_null<Scalar<SpinWeighted<ComplexDataVector, 0>>*>
         evolution_st_theta,
@@ -148,30 +148,31 @@ void GaugeAdjustedBoundaryValue<Tags::BondiSTTheta>::apply(
     const Scalar<SpinWeighted<ComplexDataVector, 0>>& omega,
     const Scalar<SpinWeighted<ComplexDataVector, 0>>& du_omega,
     const Scalar<SpinWeighted<ComplexDataVector, 0>>& du_r_divided_by_r,
+    const Scalar<SpinWeighted<ComplexDataVector, 1>>& eth_r_divided_by_r,
     const Spectral::Swsh::SwshInterpolator& interpolator, const size_t l_max,
     const Scalar<SpinWeighted<ComplexDataVector, 0>>& bondi_beta,
-    const Scalar<SpinWeighted<ComplexDataVector, 0>>& volume_psi) {
+    const Scalar<SpinWeighted<ComplexDataVector, 0>>& volume_psi,
+    const Scalar<SpinWeighted<ComplexDataVector, 0>>& dy_psi) {
   interpolator.interpolate(make_not_null(&get(*evolution_st_theta)),
                            get(cauchy_st_theta));
 
   interpolator.interpolate(make_not_null(&get(*evolution_st_psi)),
                            get(cauchy_st_psi));
 
-  const auto eth_psi =
-      Spectral::Swsh::angular_derivative<Spectral::Swsh::Tags::Eth>(
-          l_max, 1, get(*evolution_st_psi));
+  SpinWeighted<ComplexDataVector, 1> eth_psi;
+  SpinWeighted<ComplexDataVector, 1> eth_omega;
+  const SpinWeighted<ComplexDataVector, 0> surface_psi;
+  make_const_view(make_not_null(&surface_psi), get(volume_psi), 0,
+                  Spectral::Swsh::number_of_swsh_collocation_points(l_max));
+  Spectral::Swsh::angular_derivatives<
+      tmpl::list<Spectral::Swsh::Tags::Eth, Spectral::Swsh::Tags::Eth>>(
+      l_max, 1, make_not_null(&eth_psi), make_not_null(&eth_omega), surface_psi,
+      get(omega));
 
   get(*evolution_st_theta).data() +=
       real(get(evolution_gauge_u_at_scri).data() * conj(eth_psi).data());
 
-  //   -get(cauchy_st_psi) * exp(-4. * get(bondi_beta)) / get(bondi_r) +
-  //   0.5 * exp(2. * get(bondi_beta)) * eth_ethbar_psi;
-
-  const SpinWeighted<ComplexDataVector, 0> surface_psi;
-  make_const_view(make_not_null(&surface_psi), get(volume_psi), 0,
-                  Spectral::Swsh::number_of_swsh_collocation_points(l_max));
-
-  hihihi::compute_norm<0>(get(*evolution_st_psi), surface_psi);
+  hihihi::compute_norm<0>(surface_psi, get(*evolution_st_psi));
 }
 
 void GaugeAdjustedBoundaryValue<Tags::BondiQ>::apply_impl(
