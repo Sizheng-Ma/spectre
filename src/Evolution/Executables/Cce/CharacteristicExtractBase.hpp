fa9c6d1de952f5e52b6932a2db2f4b5f07a49dbb
Sizheng Ma <sma@caltech.edu>
2023-10-09 12:43:20 -0400

    dump WxxForSpECTest

--- src/Evolution/Executables/Cce/CharacteristicExtractBase.hpp
+++ src/Evolution/Executables/Cce/CharacteristicExtractBase.hpp
@@ -66,15 +66,15 @@ struct CharacteristicExtractDefaults {
       Cce::all_boundary_pre_swsh_derivative_tags_for_scri,
       Cce::all_boundary_swsh_derivative_tags_for_scri>>;
 
-  using scri_values_to_observe =
-      tmpl::list<Cce::Tags::News, Cce::Tags::ScriPlus<Cce::Tags::Strain>,
-                 Cce::Tags::ScriPlus<Cce::Tags::Psi3>,
-                 Cce::Tags::ScriPlus<Cce::Tags::Psi2>,
-                 Cce::Tags::ScriPlus<Cce::Tags::Psi1>,
-                 Cce::Tags::ScriPlus<Cce::Tags::Psi0>,
-                 Cce::Tags::Du<Cce::Tags::TimeIntegral<
-                     Cce::Tags::ScriPlus<Cce::Tags::Psi4>>>,
-                 Cce::Tags::EthInertialRetardedTime>;
+  using scri_values_to_observe = tmpl::list<
+      Cce::Tags::News, Cce::Tags::ScriPlus<Cce::Tags::Strain>,
+      Cce::Tags::ScriPlus<Cce::Tags::Psi3>,
+      Cce::Tags::ScriPlus<Cce::Tags::Psi2>,
+      Cce::Tags::ScriPlus<Cce::Tags::Psi1>, Cce::Tags::WxxForSpECTest,
+      Cce::Tags::ScriPlus<Cce::Tags::Psi0>,
+      Cce::Tags::Du<
+          Cce::Tags::TimeIntegral<Cce::Tags::ScriPlus<Cce::Tags::Psi4>>>,
+      Cce::Tags::EthInertialRetardedTime>;
 
   using cce_scri_tags =
       tmpl::list<Cce::Tags::News, Cce::Tags::ScriPlus<Cce::Tags::Strain>,
