232ea1d8cc8dafb074b1fee74a438f7086d4f07a
Sizheng Ma <masizheng13@gmail.com>
2023-11-11 14:00:58 -0800

    wt for linearized bs

--- src/Evolution/Systems/Cce/AnalyticSolutions/LinearizedBondiSachs.cpp
+++ src/Evolution/Systems/Cce/AnalyticSolutions/LinearizedBondiSachs.cpp
@@ -581,12 +581,17 @@ void LinearizedBondiSachs::dt_spherical_metric(
 void LinearizedBondiSachs::variables_impl(
     gsl::not_null<Scalar<SpinWeighted<ComplexDataVector, 0>>*> st_psi,
     size_t output_l_max, double time,
-    tmpl::type_<Tags::BondiSTPsi> /*meta*/) const {}
+    tmpl::type_<Tags::BondiSTPsi> /*meta*/) const {
+        get(*st_psi).data()=sin(time);
+    }
 
 void LinearizedBondiSachs::variables_impl(
     gsl::not_null<Scalar<SpinWeighted<ComplexDataVector, 0>>*> st_psi,
     size_t output_l_max, double time,
-    tmpl::type_<Tags::BondiSTTheta> /*meta*/) const {}
+    tmpl::type_<Tags::BondiSTTheta> /*meta*/) const {
+        get(*st_psi).data()=cos(time);
+
+    }
 
 void LinearizedBondiSachs::variables_impl(
     const gsl::not_null<Scalar<SpinWeighted<ComplexDataVector, -2>>*> news,
