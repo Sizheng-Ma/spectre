592bd83eb1babdd11d3929087622f5032a8a0dd3
Sizheng Ma <sma@caltech.edu>
2024-02-04 12:13:49 -0500

    Add hello_world

---
+++ src/hello_world/hello_world_test.hpp
@@ -0,0 +1,36 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <complex>
+#include <vector>
+
+#include "Evolution/Systems/Cce/Tags.hpp"
+
+void ccm_functions11(
+    std::vector<double>& re_h, std::vector<double>& im_h,
+    std::vector<double>& dt_cauchy_x, std::vector<double>& dt_cauchy_y,
+    std::vector<double>& dt_cauchy_z, std::vector<double>& dt_u_scri,
+    const size_t l_max, const size_t number_of_radial_points,
+    const double radius, const std::vector<double>& re_j,
+    const std::vector<double>& im_j,
+    const std::vector<std::vector<double>>& cauchy_cart,
+    const std::vector<std::vector<double>>& inertial_cart,
+    const Cce::Tags::BoundaryValue<Cce::Tags::BondiBeta>::type& cce_bondi_beta,
+    const Cce::Tags::BoundaryValue<Cce::Tags::Dr<Cce::Tags::BondiJ>>::type&
+        cce_bondi_dr_j,
+    const Cce::Tags::BoundaryValue<Cce::Tags::Du<Cce::Tags::BondiR>>::type&
+        cce_du_R,
+    const Cce::Tags::BoundaryValue<Cce::Tags::BondiH>::type& cce_bondi_h,
+    const Cce::Tags::BoundaryValue<Cce::Tags::BondiJ>::type& cce_bondi_j,
+    const Cce::Tags::BoundaryValue<Cce::Tags::BondiQ>::type& cce_bondi_q,
+    const Cce::Tags::BoundaryValue<Cce::Tags::BondiR>::type& cce_bondi_R,
+    const Cce::Tags::BoundaryValue<Cce::Tags::BondiU>::type& cce_bondi_u,
+    const Cce::Tags::BoundaryValue<Cce::Tags::BondiW>::type& cce_bondi_w,
+    const Cce::Tags::BoundaryValue<Cce::Tags::Dr<Cce::Tags::BondiU>>::type&
+        cce_dr_u,
+    const Cce::Tags::BoundaryValue<Cce::Tags::Du<Cce::Tags::BondiJ>>::type&
+        cce_du_j,
+    const Cce::Tags::BoundaryValue<Cce::Tags::DuRDividedByR>::type&
+        bondi_du_r_bdry_DuRDividedByR);
