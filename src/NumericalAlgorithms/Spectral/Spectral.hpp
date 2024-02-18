3663f16b6f8274b910d0f3f2091a43a1995732c3
Sizheng Ma <sma@caltech.edu>
2024-02-18 16:16:09 -0500

    Allow more radial points

--- src/NumericalAlgorithms/Spectral/Spectral.hpp
+++ src/NumericalAlgorithms/Spectral/Spectral.hpp
@@ -99,7 +99,7 @@ constexpr size_t minimum_number_of_points =
  */
 template <Basis basis>
 constexpr size_t maximum_number_of_points =
-    basis == Basis::FiniteDifference ? 40 : 20;
+    basis == Basis::FiniteDifference ? 40 : 41;
 
 /*!
  * \brief Compute the function values of the basis function \f$\Phi_k(x)\f$
