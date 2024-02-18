f0c3f22099167ddeecc56cfb420d7288175835c3
Sizheng Ma <sma@caltech.edu>
2024-02-18 16:09:38 -0500

    Add a pulse to Cce id

--- src/Evolution/Systems/Cce/Initialize/InverseCubic.cpp
+++ src/Evolution/Systems/Cce/Initialize/InverseCubic.cpp
@@ -49,6 +49,20 @@ void InverseCubic<true>::apply(
       1.0 - Spectral::collocation_points<Spectral::Basis::Legendre,
                                          Spectral::Quadrature::GaussLobatto>(
                 number_of_radial_points);
+  const size_t number_of_angular_points =
+      Spectral::Swsh::number_of_swsh_collocation_points(l_max);
+
+  Spectral::Swsh::SpinWeightedSphericalHarmonic y_22{2, 2_st, 0};
+  const auto& collocation_metadata =
+      Spectral::Swsh::cached_collocation_metadata<
+          Spectral::Swsh::ComplexRepresentation::Interleaved>(l_max);
+  SpinWeighted<ComplexDataVector, 2> perturbed_j{number_of_angular_points};
+  for (const auto collocation_point : collocation_metadata) {
+    const std::complex<double> y_22_factor =
+        y_22.evaluate(collocation_point.theta, collocation_point.phi);
+    perturbed_j.data()[collocation_point.offset] = y_22_factor;
+  }
+
   for (size_t i = 0; i < number_of_radial_points; i++) {
     ComplexDataVector angular_view_j{
         get(*j).data().data() + get(boundary_j).size() * i,
@@ -65,6 +79,14 @@ void InverseCubic<true>::apply(
     angular_view_j =
         one_minus_y_collocation[i] * one_minus_y_coefficient +
         pow<3>(one_minus_y_collocation[i]) * one_minus_y_cubed_coefficient;
+
+    double ycenter = -0.0;
+    double width = 0.15;
+    angular_view_j +=
+        perturbed_j.data() * 0.001 * std::complex(0.0, 1.0) *
+        exp(-pow(1.0 - one_minus_y_collocation[i] - ycenter, 2.0) / width /
+            width) *
+        one_minus_y_collocation[i];
   }
   Spectral::Swsh::create_angular_and_cartesian_coordinates(
       cartesian_cauchy_coordinates, angular_cauchy_coordinates, l_max);
@@ -93,6 +115,20 @@ void InverseCubic<true>::operator()(
       1.0 - Spectral::collocation_points<Spectral::Basis::Legendre,
                                          Spectral::Quadrature::GaussLobatto>(
                 number_of_radial_points);
+  const size_t number_of_angular_points =
+      Spectral::Swsh::number_of_swsh_collocation_points(l_max);
+
+  Spectral::Swsh::SpinWeightedSphericalHarmonic y_22{2, 2_st, 0};
+  const auto& collocation_metadata =
+      Spectral::Swsh::cached_collocation_metadata<
+          Spectral::Swsh::ComplexRepresentation::Interleaved>(l_max);
+  SpinWeighted<ComplexDataVector, 2> perturbed_j{number_of_angular_points};
+  for (const auto collocation_point : collocation_metadata) {
+    const std::complex<double> y_22_factor =
+        y_22.evaluate(collocation_point.theta, collocation_point.phi);
+    perturbed_j.data()[collocation_point.offset] = y_22_factor;
+  }
+
   for (size_t i = 0; i < number_of_radial_points; i++) {
     ComplexDataVector angular_view_j{
         get(*j).data().data() + get(boundary_j).size() * i,
@@ -109,6 +145,14 @@ void InverseCubic<true>::operator()(
     angular_view_j =
         one_minus_y_collocation[i] * one_minus_y_coefficient +
         pow<3>(one_minus_y_collocation[i]) * one_minus_y_cubed_coefficient;
+
+    double ycenter = -0.0;
+    double width = 0.15;
+    angular_view_j +=
+        perturbed_j.data() * 0.001 * std::complex(0.0, 1.0) *
+        exp(-pow(1.0 - one_minus_y_collocation[i] - ycenter, 2.0) / width /
+            width) *
+        one_minus_y_collocation[i];
   }
   Spectral::Swsh::create_angular_and_cartesian_coordinates(
       cartesian_cauchy_coordinates, angular_cauchy_coordinates, l_max);
