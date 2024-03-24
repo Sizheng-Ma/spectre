9911fac4639e365166353608747d3acac5add34c
Sizheng Ma <sma@caltech.edu>
2024-03-23 23:46:29 -0400

    Add KleinGordonAnalyticBoundaryDataManager

--- src/Evolution/Systems/Cce/AnalyticBoundaryDataManager.hpp
+++ src/Evolution/Systems/Cce/AnalyticBoundaryDataManager.hpp
@@ -115,4 +115,44 @@ void AnalyticBoundaryDataManager::write_news(
       observer_proxy, "/Cce/News_" + prefix + "expected"s, file_legend,
       std::make_tuple(data_to_write));
 }
+
+class KleinGordonAnalyticBoundaryDataManager {
+ public:
+  // charm needs an empty constructor.
+  KleinGordonAnalyticBoundaryDataManager() = default;
+
+  KleinGordonAnalyticBoundaryDataManager(
+      size_t l_max, double extraction_radius,
+      std::unique_ptr<Solutions::KleinGordonWorldtubeData> generator);
+
+  /*!
+   * \brief Update the `boundary_data_variables` entries for all tags in
+   * `Tags::characteristic_worldtube_boundary_tags` to the boundary data from
+   * the analytic solution at  `time`.
+   *
+   * \details This class retrieves metric boundary data from the
+   * `Cce::Solutions::WorldtubeData` derived class that represents an analytic
+   * solution, then dispatches to `Cce::create_bondi_boundary_data()` to
+   * construct the Bondi values into the provided `Variables`
+   */
+  bool populate_hypersurface_boundary_data(
+      gsl::not_null<Variables<Tags::klein_gordon_worldtube_boundary_tags>*>
+          boundary_data_variables,
+      double time) const;
+
+  size_t get_l_max() const { return l_max_; }
+
+  const Solutions::KleinGordonWorldtubeData& get_generator() const {
+    return *generator_;
+  }
+
+  /// Serialization for Charm++.
+  // NOLINTNEXTLINE(google-runtime-references)
+  void pup(PUP::er& p);
+
+ private:
+  size_t l_max_ = 0;
+  std::unique_ptr<Solutions::KleinGordonWorldtubeData> generator_;
+  double extraction_radius_ = std::numeric_limits<double>::signaling_NaN();
+};
 }  // namespace Cce
