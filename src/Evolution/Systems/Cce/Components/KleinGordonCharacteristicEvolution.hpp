5a264cbd92a0778d2fd897be80c3f116abc0efcd
Sizheng Ma <masizheng13@gmail.com>
2024-03-31 17:22:34 -0400

    ID for KleinGordonAnalyticWorldtubeBoundary

--- src/Evolution/Systems/Cce/Components/KleinGordonCharacteristicEvolution.hpp
+++ src/Evolution/Systems/Cce/Components/KleinGordonCharacteristicEvolution.hpp
@@ -92,7 +92,8 @@ struct KleinGordonCharacteristicEvolution
       // iterations immediately following restarts
       Actions::InitializeFirstHypersurface<
           evolve_ccm, typename Metavariables::cce_boundary_component>,
-      Actions::InitializeKleinGordonFirstHypersurface,
+      Actions::InitializeKleinGordonFirstHypersurface<
+          typename Metavariables::cce_boundary_component>,
       tmpl::conditional_t<
           tt::is_a_v<AnalyticWorldtubeBoundary,
                      typename Metavariables::cce_boundary_component>,
@@ -136,7 +137,8 @@ struct KleinGordonCharacteristicEvolution
           typename Metavariables::klein_gordon_boundary_communication_tags>,
       Actions::InitializeFirstHypersurface<
           evolve_ccm, typename Metavariables::cce_boundary_component>,
-      Actions::InitializeKleinGordonFirstHypersurface,
+      Actions::InitializeKleinGordonFirstHypersurface<
+          typename Metavariables::cce_boundary_component>,
       tmpl::conditional_t<
           tt::is_a_v<AnalyticWorldtubeBoundary,
                      typename Metavariables::cce_boundary_component>,
