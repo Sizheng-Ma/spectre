5880248094a330391474e6cbbf395383ce213b45
Sizheng Ma <masizheng13@gmail.com>
2024-03-31 17:22:34 -0400

    ID for KleinGordonAnalyticWorldtubeBoundary

--- src/Evolution/Systems/Cce/Actions/InitializeKleinGordonFirstHypersurface.hpp
+++ src/Evolution/Systems/Cce/Actions/InitializeKleinGordonFirstHypersurface.hpp
@@ -3,6 +3,8 @@
 
 #pragma once
 
+#include "Evolution/Systems/Cce/AnalyticSolutions/KleinGordonWaveHelper.hpp"
+#include "Evolution/Systems/Cce/Components/WorldtubeBoundary.hpp"
 #include "Evolution/Systems/Cce/OptionTags.hpp"
 #include "Evolution/Systems/Cce/Tags.hpp"
 #include "NumericalAlgorithms/Spectral/Spectral.hpp"
@@ -24,6 +26,7 @@ namespace Cce::Actions {
  * an initialization phase or early (before a `Actions::Goto` loop or similar)
  * in the `Evolve` phase.
  */
+template <typename BoundaryComponent>
 struct InitializeKleinGordonFirstHypersurface {
   using const_global_cache_tags =
       tmpl::list<Tags::LMax, Tags::NumberOfRadialPoints>;
@@ -46,33 +49,67 @@ struct InitializeKleinGordonFirstHypersurface {
       return {Parallel::AlgorithmExecution::Continue, std::nullopt};
     }
 
-    db::mutate<Tags::KleinGordonPsi>(
-        [](const gsl::not_null<Scalar<SpinWeighted<ComplexDataVector, 0>>*>
-               kg_psi_volume,
-           const Scalar<SpinWeighted<ComplexDataVector, 0>>& boundary_kg_psi,
-           const size_t l_max, const size_t number_of_radial_points) {
-          const DataVector one_minus_y_collocation =
-              1.0 -
-              Spectral::collocation_points<Spectral::Basis::Legendre,
-                                           Spectral::Quadrature::GaussLobatto>(
-                  number_of_radial_points);
+    if constexpr (tt::is_a_v<KleinGordonAnalyticWorldtubeBoundary,
+                             BoundaryComponent>) {
+      db::mutate<Tags::KleinGordonPsi>(
+          [](const gsl::not_null<Scalar<SpinWeighted<ComplexDataVector, 0>>*>
+                 kg_psi_volume,
+             const Scalar<SpinWeighted<ComplexDataVector, 0>> bondi_r,
+             const size_t l_max, const size_t number_of_radial_points) {
+            const DataVector one_minus_y_collocation =
+                1.0 - Spectral::collocation_points<
+                          Spectral::Basis::Legendre,
+                          Spectral::Quadrature::GaussLobatto>(
+                          number_of_radial_points);
 
-          const size_t boundary_size =
-              Spectral::Swsh::number_of_swsh_collocation_points(l_max);
+            const size_t boundary_size =
+                Spectral::Swsh::number_of_swsh_collocation_points(l_max);
 
-          for (size_t i = 0; i < number_of_radial_points; i++) {
-            ComplexDataVector angular_view_kg_psi{
-                get(*kg_psi_volume).data().data() + boundary_size * i,
-                boundary_size};
+            for (size_t i = 0; i < number_of_radial_points; i++) {
+              ComplexDataVector angular_view_kg_psi{
+                  get(*kg_psi_volume).data().data() + boundary_size * i,
+                  boundary_size};
 
-            // this gives psi = (boundary value) / r
-            angular_view_kg_psi =
-                get(boundary_kg_psi).data() * one_minus_y_collocation[i] / 2.;
-          }
-        },
-        make_not_null(&box),
-        db::get<Tags::BoundaryValue<Tags::KleinGordonPsi>>(box),
-        db::get<Tags::LMax>(box), db::get<Tags::NumberOfRadialPoints>(box));
+              auto u0new = real(-get(bondi_r).data() -
+                                4 * log(get(bondi_r).data() / 2. - 1.));
+              auto one_over_r =
+                  one_minus_y_collocation[i] / 2. / real(get(bondi_r).data());
+
+              Solutions::KleinGordon::bc_psi(
+                  make_not_null(&angular_view_kg_psi), u0new, one_over_r);
+            }
+          },
+          make_not_null(&box), db::get<Tags::BoundaryValue<Tags::BondiR>>(box),
+          db::get<Tags::LMax>(box), db::get<Tags::NumberOfRadialPoints>(box));
+    } else {
+      db::mutate<Tags::KleinGordonPsi>(
+          [](const gsl::not_null<Scalar<SpinWeighted<ComplexDataVector, 0>>*>
+                 kg_psi_volume,
+             const Scalar<SpinWeighted<ComplexDataVector, 0>>& boundary_kg_psi,
+             const size_t l_max, const size_t number_of_radial_points) {
+            const DataVector one_minus_y_collocation =
+                1.0 - Spectral::collocation_points<
+                          Spectral::Basis::Legendre,
+                          Spectral::Quadrature::GaussLobatto>(
+                          number_of_radial_points);
+
+            const size_t boundary_size =
+                Spectral::Swsh::number_of_swsh_collocation_points(l_max);
+
+            for (size_t i = 0; i < number_of_radial_points; i++) {
+              ComplexDataVector angular_view_kg_psi{
+                  get(*kg_psi_volume).data().data() + boundary_size * i,
+                  boundary_size};
+
+              // this gives psi = (boundary value) / r
+              angular_view_kg_psi =
+                  get(boundary_kg_psi).data() * one_minus_y_collocation[i] / 2.;
+            }
+          },
+          make_not_null(&box),
+          db::get<Tags::BoundaryValue<Tags::KleinGordonPsi>>(box),
+          db::get<Tags::LMax>(box), db::get<Tags::NumberOfRadialPoints>(box));
+    }
 
     return {Parallel::AlgorithmExecution::Continue, std::nullopt};
   }
--- src/Evolution/Systems/Cce/Components/KleinGordonCharacteristicEvolution.hpp
+++ src/Evolution/Systems/Cce/Components/KleinGordonCharacteristicEvolution.hpp
@@ -92,7 +92,8 @@ struct KleinGordonCharacteristicEvolution
       // iterations immediately following restarts
       Actions::InitializeFirstHypersurface<
           evolve_ccm, typename Metavariables::cce_boundary_component>,
-      Actions::InitializeKleinGordonFirstHypersurface,
+      Actions::InitializeKleinGordonFirstHypersurface<
+          typename Metavariables::cce_boundary_component>,
       tmpl::conditional_t<
           tt::is_a_v<AnalyticWorldtubeBoundary,
                      typename Metavariables::cce_boundary_component>,
@@ -135,7 +136,8 @@ struct KleinGordonCharacteristicEvolution
           typename Metavariables::klein_gordon_boundary_communication_tags>,
       Actions::InitializeFirstHypersurface<
           evolve_ccm, typename Metavariables::cce_boundary_component>,
-      Actions::InitializeKleinGordonFirstHypersurface,
+      Actions::InitializeKleinGordonFirstHypersurface<
+          typename Metavariables::cce_boundary_component>,
       tmpl::conditional_t<
           tt::is_a_v<AnalyticWorldtubeBoundary,
                      typename Metavariables::cce_boundary_component>,
