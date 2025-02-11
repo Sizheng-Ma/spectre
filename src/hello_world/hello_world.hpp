58af2f7100f332dd7caa83cf6b0263f9ef883a7f
Sizheng Ma <sma@caltech.edu>
2025-02-10 23:28:01 -0500

    ccm_interpolation0 takes real

--- src/hello_world/hello_world.hpp
+++ src/hello_world/hello_world.hpp
@@ -61,11 +61,11 @@ void ccm_interpolation(std::vector<std::complex<double>>& psi0_ccm_interpolated,
                        const size_t l_max,
                        const std::vector<std::complex<double>>& psi0_ccm);
 
-void ccm_interpolation0(std::vector<std::complex<double>>& psi0_ccm_interpolated,
-                       const std::vector<double>& cauchy_theta,
-                       const std::vector<double>& cauchy_phi,
-                       const size_t l_max,
-                       const std::vector<std::complex<double>>& psi0_ccm);
+void ccm_interpolation0(
+    std::vector<std::complex<double>>& psi0_ccm_interpolated,
+    const std::vector<double>& cauchy_theta,
+    const std::vector<double>& cauchy_phi, const size_t l_max,
+    const std::vector<double>& psi0_ccm);
 
 namespace spectre {
 struct MyScriPlusInterpolationManager;
